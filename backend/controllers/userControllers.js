8ad7324128cfafef7013c1faa2ad4593a73c76d9
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2025-05-01 10:41:53 +0300

    Custom Error Middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -6,4 +6,25 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
+// @desc Register a new user
+// route POST /api/users/register
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc   Get user profile
+// route   POST /api/users/profile
+// @access private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc Logout user
+// route POST /api/users/logout
+// @access Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
 export { authUser };
