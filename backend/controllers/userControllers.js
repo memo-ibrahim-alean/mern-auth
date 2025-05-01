8e87d946f42848480932a9908faae20ab8daabc4
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2025-05-01 10:58:52 +0300

    User Controller Functions

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -6,8 +6,8 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
-// @desc Register a new user
-// route POST /api/users/register
+// @desc   Register a new user
+// route   POST /api/users/register
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Register User" });
@@ -17,14 +17,27 @@ const registerUser = asyncHandler(async (req, res) => {
 // route   POST /api/users/profile
 // @access private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+  res.status(200).json({ message: "User profile" });
 });
 
-// @desc Logout user
-// route POST /api/users/logout
-// @access Public
+// @desc   Logout user
+// route   POST /api/users/logout
+// @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+  res.status(200).json({ message: "logout user" });
 });
 
-export { authUser };
+// @desc   Update user profile
+// route   PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update user profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  getUserProfile,
+  logoutUser,
+  updateUserProfile,
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,17 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controllers/userControllers.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  logoutUser,
+  updateUserProfile,
+} from "../controllers/userControllers.js";
 
+router.post("/", registerUser);
 router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.post("/register", registerUser);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 export default router;
